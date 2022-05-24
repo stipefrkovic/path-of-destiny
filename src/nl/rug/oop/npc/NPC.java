58d3e48bfc0eccc8fbc331c2f787278cb32d9633
Jonas Scholz <jojo.scholz@gmail.com>
2022-05-24 12:26:37 +0200

    Added a few attributes to the NPC class

--- src/nl/rug/oop/npc/NPC.java
+++ src/nl/rug/oop/npc/NPC.java
@@ -2,4 +2,26 @@ package nl.rug.oop.npc;
 
 public abstract class NPC {
 
+    private String name;
+    private String type;
+    private int health;
+    private float strength;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public float getStrength() {
+        return strength;
+    }
+
+    
 }
