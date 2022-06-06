98c7da2ac37d9161f528835b1552ba114e736128
Jonas Scholz <jojo.scholz@gmail.com>
2022-06-06 11:23:27 +0200

    Added Bosses

--- src/nl/rug/oop/npc/SpiderBoss.java
+++ src/nl/rug/oop/npc/SpiderBoss.java
@@ -1,5 +1,6 @@
 package nl.rug.oop.npc;
 
+import nl.rug.oop.effects.PoisonEffect;
 import nl.rug.oop.player.Player;
 import nl.rug.oop.scene.Action;
 import nl.rug.oop.scene.Scene;
@@ -8,22 +9,23 @@ import java.util.HashMap;
 
 public class SpiderBoss extends BossNPC{
 
-    public SpiderBoss(Dialogue dialogue) {
+    public SpiderBoss() {
         super("Wren", "Weaver", 200, 8, new Dialogue("You will never defeat me.", new HashMap<>(), 0));
     }
 
     @Override
     protected String onFirstBelow75Health(Player player, Scene currentScene, Action action) {
-        return "";
+        return player.addEffect(new PoisonEffect());
     }
 
     @Override
     protected String onFirstBelow50Health(Player player, Scene currentScene, Action action) {
-        return "";
+        return player.addEffect(new PoisonEffect());
     }
 
     @Override
     protected String onFirstBelow25Health(Player player, Scene currentScene, Action action) {
-        return "";
+        this.strength *= 1.5;
+        return this.getName()+" is enraged. ";
     }
 }
