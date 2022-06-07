be5e353e9ec632ae19bcad1c99bef95328dc3dec
Andro Erdelez <andro.erdelez@gmail.com>
2022-06-07 21:04:48 +0200

    Lost all my code, start/save/load implemented

--- src/nl/rug/oop/model/RpgGame.java
+++ src/nl/rug/oop/model/RpgGame.java
@@ -1,12 +1,22 @@
 package nl.rug.oop.model;
 
+import nl.rug.oop.player.Player;
+import nl.rug.oop.scene.Scene;
+
 import java.beans.PropertyChangeListener;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class RpgGame {
-
+    private Scene scene;
+    private Player player;
     Collection<PropertyChangeListener> listeners = new ArrayList<>();
+
+    public RpgGame(Scene scene, Player player) {
+        this.scene = scene;
+        this.player = player;
+    }
 
     /**
      * Method adds a listener to the array of listeners in the model.
@@ -14,5 +24,42 @@ public class RpgGame {
      */
     public void addListener(PropertyChangeListener listener) {
         listeners.add(listener);
+    }
+
+    /**
+     * Starts a new game
+     * @param scene Initial scene of a new game
+     * @param player Player class picked by a user
+     * @return Game (RpgGame object) with the initial scene and picked player class
+     */
+    public static RpgGame startGame(Scene scene, Player player) {
+        return new RpgGame(scene, player);
+    }
+
+    /**
+     * Saves a game (RpgGame object) to a file called game.obj
+     * @param game Game to save
+     */
+    public static void saveGame(RpgGame game) {
+        try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("game.obj"))) {
+            output.writeObject(game);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Loads a game (RpgGame object) from a file called game.obj
+     * @return Game loaded from the file
+     */
+    public static RpgGame loadGame() {
+        RpgGame game = null;
+        try (ObjectInputStream input = new ObjectInputStream(new FileInputStream("game.obj"))) {
+            game = (RpgGame) input.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return game;
     }
 }
